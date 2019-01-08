6729cb9bedcc1468bc567187cc4b96f47862c20e
Ayan Mullick <ayanmullick@gmail.com>
2019-01-08 13:04:36 -0500

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,4 +1,4 @@
-Install-Module AzurePSDrive,SHIPS
+#Install-Module AzurePSDrive,SHIPS
 Import-Module AzurePSDrive
 New-PSDrive -Name Azure -PSProvider SHiPS -root 'AzurePSDrive#Azure'
 Set-Location Azure:
