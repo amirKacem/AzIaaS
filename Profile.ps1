999bb0a67d0ebfa4bd928c955df92abb945aefbd
Ayan Mullick <ayanmullick@gmail.com>
2021-01-03 19:22:06 -0600

    Delete Profile.ps1

--- Profile.ps1
+++
@@ -1,8 +0,0 @@
-#Install-Module AzurePSDrive,SHIPS
-Import-Module AzurePSDrive
-New-PSDrive -Name Azure -PSProvider SHiPS -root 'AzurePSDrive#Azure'
-Set-Location Azure:
-
-
-
-#Installs Profile--->   Invoke-Expression (Invoke-WebRequest -UseBasicParsing http://bit.ly/ayanProfile)  or  https://git.io/fhGoi
