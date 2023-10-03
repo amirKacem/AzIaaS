fab4fbf96b917be3466ea14609a778b309c4df27
Ayan Mullick <ayanmullick@gmail.com>
2023-10-03 18:47:49 -0500

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  marked.use({ break: true });
+
+  function renderFileContent(markdownFileUrl) {
+    renderMarkDownFile(markdownFileUrl);
+  }
+
+  function renderMarkDownFile(fileUrl) {
+    return fetch(fileUrl)
+      .then((response) => response.text())
+      .then((markdown) => {
+        document.getElementById("content").innerHTML = marked.parse(markdown);
+        var scriptTags = document.querySelectorAll("#content script");
+
+        try {
+          scriptTags.forEach(function (scriptTag) {
+            if (scriptTag.src) {
+              var externalScript = document.createElement("script");
+              externalScript.src = scriptTag.src;
+              document.getElementById("content").removeChild(scriptTag);
+              document.getElementById("content").appendChild(externalScript);
+            } else {
+              eval(scriptTag.innerText);
+            }
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      });
+  }
+
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const path = urlParams.get("path");
+  if (path !== "" && path !== null) {
+    renderFileContent(path);
+  } 
+});
