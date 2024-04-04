30a9b9bc5bc5aceb9a8492ac4215599d88b5fcb6
Ayan <ayanmullick@gmail.com>
2024-04-04 00:13:27 -0500

    .

--- 3TierApp/GovResource.ps1
+++ 3TierApp/GovResource.ps1
@@ -10,12 +10,11 @@ function Get-Suffix {
 $RG             = Get-AzResourceGroup -Name GitHubAction24
 $AzParams       = @{Location = 'NorthCentralUS'; ResourceGroupName  = $RG.ResourceGroupName; Verbose=$true}
 
-$Identity       = New-AzUserAssignedIdentity @AzParams -Name ('tiered' + (Get-Suffix))
 $SaParams       = @{SkuName = 'Standard_LRS'; Kind = 'StorageV2'; AccessTier = 'Hot'; EnableHierarchicalNamespace = $true}
 $SaNwParams     = @{EnableHttpsTrafficOnly = $true; MinimumTlsVersion = 'TLS1_2';  }
-$SaSecParams    = @{AllowSharedKeyAccess  = $false; AssignIdentity = $true; UserAssignedIdentityId = $Identity.Id } 
+$SaSecParams    = @{AllowSharedKeyAccess  = $false } 
 New-AzStorageAccount -Name $('tiered' + (Get-Suffix)).ToLower() @AzParams @SaParams @SaNwParams @SaSecParams
 
-
+#New-AzUserAssignedIdentity @AzParams -Name ('tiered' + (Get-Suffix))
 #New-AzOperationalInsightsWorkspace -Name ('3Tier' + (Get-Suffix)) @AzParams -RetentionInDays 30 
 #New-AzKeyvault -Name ('Tiered' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
