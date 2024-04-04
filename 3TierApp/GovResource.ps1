7b91624da18e9c987ee6e4320cccdaaca3e59929
Ayan <ayanmullick@gmail.com>
2024-04-03 21:46:19 -0500

    .

--- 3TierApp/GovResource.ps1
+++ 3TierApp/GovResource.ps1
@@ -10,12 +10,12 @@ function Get-Suffix {
 $RG             = Get-AzResourceGroup -Name GitHubAction24
 $AzParams       = @{Location = 'NorthCentralUS'; ResourceGroupName  = $RG.ResourceGroupName; Verbose=$true}
 <#
-$Identity       = New-AzUserAssignedIdentity @AzParams -Name ('Func' + (Get-Suffix))
+$Identity       = New-AzUserAssignedIdentity @AzParams -Name ('3Tier' + (Get-Suffix))
 $SaParams       = @{SkuName = 'Standard_LRS'; Kind = 'StorageV2'; AccessTier = 'Hot'; LargeFileSharesState = 'Disabled'; EnableHierarchicalNamespace = $true}
 $SaNwParams     = @{EnableHttpsTrafficOnly = $true; MinimumTlsVersion = 'TLS1_2'; EncryptionServices = 'Blob'; AllowBlobPublicAccess = 'Disabled'; }
 $SaSecParams    = @{AllowStorageAccountKeyAccess = 'Disabled'; AllowSharedAccessSignatureExpiryIntervalInYear = 1; EnableAzureActiveDirectoryAuthorization = $true; UserAssignedIdentityId = $Identity.Id}
-New-AzStorageAccount -Name ('Func' + (Get-Suffix)) @AzParams @SaParams @SaNwParams @SaSecParams
+New-AzStorageAccount -Name ('3Tier' + (Get-Suffix)) @AzParams @SaParams @SaNwParams @SaSecParams
 #>
 
-#New-AzOperationalInsightsWorkspace -Name ('Func' + (Get-Suffix)) @AzParams -RetentionInDays 30 
-New-AzKeyvault -Name ('Func' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
+#New-AzOperationalInsightsWorkspace -Name ('3Tier' + (Get-Suffix)) @AzParams -RetentionInDays 30 
+New-AzKeyvault -Name ('3Tier' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
