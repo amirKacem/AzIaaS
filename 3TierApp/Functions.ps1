6d2b12579c2fa4369a0d2d97191806f18f486f4d
Ayan <ayanmullick@gmail.com>
2024-04-04 23:27:19 -0500

    .

---
+++ 3TierApp/Functions.ps1
@@ -0,0 +1,20 @@
+function Get-Suffix {
+    $ast        = ([System.Management.Automation.Language.Parser]::ParseInput($MyInvocation.Line, [ref]$null, [ref]$null)).EndBlock.Statements[0]
+    $pipeline   = $ast.left ? $ast.right.PipelineElements : $ast.PipelineElements
+    $commandName= $pipeline[0].commandElements[0].Value
+    $command    = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All') ?? {throw "Command not found: $commandName"}
+    
+    $command.Noun.TrimStart('A') -creplace '[^A-Z]'
+                    }
+
+
+function Convert-MdTableToPSObject {param([Parameter(Mandatory=$true)][string]$MarkdownFilePath)
+    $lines = (Get-Content -Path $MarkdownFilePath -Raw) -replace "`r`n", "`n" -split "`n"
+    $headers = (($lines[0] -split '\|').Where({ $_.Trim() -ne '' })).Trim()
+    ($lines[2..$lines.Length]).Where({ $_ -ne '' }) | ForEach-Object {
+        $properties = [ordered]@{}
+        $cells = (($_ -split '\|').Where({ $_.Trim() -ne '' })).Trim()
+        for ($i = 0; $i -lt $headers.Count; $i++) { $properties[$headers[$i]] = $cells[$i] }
+        [PSCustomObject]$properties
+    }
+}                    
--- 3TierApp/GovResource.ps1
+++ 3TierApp/GovResource.ps1
@@ -1,20 +1,11 @@
-function Get-Suffix {
-    $ast        = ([System.Management.Automation.Language.Parser]::ParseInput($MyInvocation.Line, [ref]$null, [ref]$null)).EndBlock.Statements[0]
-    $pipeline   = $ast.left ? $ast.right.PipelineElements : $ast.PipelineElements
-    $commandName= $pipeline[0].commandElements[0].Value
-    $command    = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All') ?? {throw "Command not found: $commandName"}
-    
-    $command.Noun.TrimStart('A') -creplace '[^A-Z]'
-                    }
-
 $RG             = Get-AzResourceGroup -Name GitHubAction24
 $AzParams       = @{Location = 'NorthCentralUS'; ResourceGroupName  = $RG.ResourceGroupName; Verbose=$true}
-<#
+
 $SaParams       = @{SkuName = 'Standard_LRS'; Kind = 'StorageV2'; AccessTier = 'Hot'; EnableHierarchicalNamespace = $true}
 $SaNwParams     = @{EnableHttpsTrafficOnly = $true; MinimumTlsVersion = 'TLS1_2';  }
 $SaSecParams    = @{AllowSharedKeyAccess  = $false } 
 New-AzStorageAccount -Name $('tiered' + (Get-Suffix)).ToLower() @AzParams @SaParams @SaNwParams @SaSecParams
-#>
-#New-AzUserAssignedIdentity @AzParams -Name ('tiered' + (Get-Suffix))
-#New-AzOperationalInsightsWorkspace -Name ('3Tier' + (Get-Suffix)) @AzParams -RetentionInDays 30 
-#New-AzKeyvault -Name ('Tiered' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
+
+New-AzUserAssignedIdentity @AzParams -Name ('tiered' + (Get-Suffix))
+New-AzOperationalInsightsWorkspace -Name ('3Tier' + (Get-Suffix)) @AzParams -RetentionInDays 30 
+New-AzKeyvault -Name ('Tiered' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
