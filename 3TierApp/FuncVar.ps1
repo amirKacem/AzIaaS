e72a609a213eb3c68a46f3e2cada604a1be89f19
Ayan <ayanmullick@gmail.com>
2024-04-04 23:05:38 -0500

    .

--- 3TierApp/FuncVar.ps1
+++ 3TierApp/FuncVar.ps1
@@ -1,7 +1,8 @@
 function Get-CurrentTime { (Get-Date).ToString("hh:mm:ss tt") }
 
-
+<#
 $RG = Get-AzResourceGroup -Name GitHubAction24
 $rgName = $RG.ResourceGroupName
 
 Write-Output "RG_NAME=$rgName" >> $Env:GITHUB_ENV  #Persists only across 'steps' in the same job. And is deleted after GHWorkflow run
+#>
