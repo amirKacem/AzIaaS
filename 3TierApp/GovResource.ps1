bab5ecd876083ad8384d2ba7801ce56d0ea9a032
Ayan <ayanmullick@gmail.com>
2024-04-05 01:47:42 -0500

    .

--- 3TierApp/GovResource.ps1
+++ 3TierApp/GovResource.ps1
@@ -9,11 +9,14 @@ New-AzStorageAccount -Name $('tiered' + (Get-Suffix)).ToLower() @AzParams @SaPar
 New-AzUserAssignedIdentity -Name ('tiered' + (Get-Suffix)) @AzParams 
 New-AzOperationalInsightsWorkspace -Name ('3Tier' + (Get-Suffix)) @AzParams -RetentionInDays 30 
 #New-AzKeyvault -Name ('Tiered1' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
+#WARNING: Access policy is not set. No user or application have access permission to use this vault. 
+#This can happen if the vault was created by a service principal. Please use Set-AzKeyVaultAccessPolicy to set access policies.
 #>
 
 $rules = Convert-MdTable2PSObject -MarkdownFilePath .\3TierApp\NsgRules.md                                  #NSG Creation
 $ConstParams = @{Access = 'Allow'; SourcePortRange = '*'; Priority = '100'; Protocol = 'Tcp'}
 $NSGRulesArray = $rules | ForEach-Object {
-    $ruleParams = @{Name = $_.Name + 'Rule'; Direction = $_.Direction; SourceAddressPrefix = $_.SourceAddressPrefix; DestinationAddressPrefix = $_.DestAddressPrefix; DestinationPortRange = $_.DestPortRange; Description = $_.Description} + $ConstParams
+    $ruleParams = @{Name = $_.Name + 'Rule'; Description = $_.Description
+    Direction = $_.Direction; SourceAddressPrefix = $_.SourceAddressPrefix; DestinationAddressPrefix = $_.DestAddressPrefix; DestinationPortRange = $_.DestPortRange } + $ConstParams
     New-AzNetworkSecurityRuleConfig @ruleParams}
 New-AzNetworkSecurityGroup -Name ('tiered' + (Get-Suffix)) @AzParams -SecurityRules $NSGRulesArray
