2ff7956a1b1dd5ef778d5983cbe897703cbedc9d
Ayan <ayanmullick@gmail.com>
2024-04-04 22:10:37 -0500

    .

--- 3TierApp/FuncVar.ps1
+++ 3TierApp/FuncVar.ps1
@@ -4,4 +4,4 @@ function Get-CurrentTime { (Get-Date).ToString("hh:mm:ss tt") }
 $RG = Get-AzResourceGroup -Name GitHubAction24
 $rgName = $RG.ResourceGroupName
 
-echo "RG_NAME=$rgName" >> $Env:GITHUB_ENV
+Write-Output "RG_NAME=$rgName" >> $Env:GITHUB_ENV
