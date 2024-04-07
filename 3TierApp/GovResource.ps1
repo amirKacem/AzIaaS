931b9044b7531bffd62269747a7125a93402bfbb
Ayan <ayanmullick@gmail.com>
2024-04-06 21:32:25 -0500

    .

--- 3TierApp/GovResource.ps1
+++ 3TierApp/GovResource.ps1
@@ -1,20 +1,16 @@
 $RG             = Get-AzResourceGroup -Name GitHubAction24
 $AzParams       = @{Location = 'NorthCentralUS'; ResourceGroupName  = $RG.ResourceGroupName; Verbose=$true}
-<#
+
 $SaParams       = @{SkuName = 'Standard_LRS'; Kind = 'StorageV2'; AccessTier = 'Hot'; EnableHierarchicalNamespace = $true}
 $SaNwParams     = @{EnableHttpsTrafficOnly = $true; MinimumTlsVersion = 'TLS1_2';  }
 $SaSecParams    = @{AllowSharedKeyAccess  = $false } 
-New-AzStorageAccount -Name $('tiered' + (Get-Suffix)).ToLower() @AzParams @SaParams @SaNwParams @SaSecParams
+$StorageAcc     = New-AzStorageAccount -Name $('tiered' + (Get-Suffix)).ToLower() @AzParams @SaParams @SaNwParams @SaSecParams
 
-New-AzUserAssignedIdentity -Name ('tiered' + (Get-Suffix)) @AzParams 
-New-AzOperationalInsightsWorkspace -Name ('3Tier' + (Get-Suffix)) @AzParams -RetentionInDays 30 
-#New-AzKeyvault -Name ('Tiered1' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
-#WARNING: Access policy is not set. No user or application have access permission to use this vault. 
-#This can happen if the vault was created by a service principal. Please use Set-AzKeyVaultAccessPolicy to set access policies.
-#>
-
+$Identity       = New-AzUserAssignedIdentity -Name ('tiered' + (Get-Suffix)) @AzParams 
+$LAW            =New-AzOperationalInsightsWorkspace -Name ('3Tier' + (Get-Suffix)) @AzParams -RetentionInDays 30 
+$KV             = New-AzKeyvault -Name ('Tiered2' + (Get-Suffix)) @AzParams -EnabledForDiskEncryption -Sku Premium -EnableRbacAuthorization -EnablePurgeProtection
 #New-AzRoleAssignment -Scope $KV.ResourceId -SignInName <> -RoleDefinitionName 'Key Vault Administrator'  #For user
-#New-AzRoleAssignment -Scope $KV.ResourceId -ObjectId $Identity.PrincipalId -RoleDefinitionName 'Key Vault Administrator' #For UAI
+New-AzRoleAssignment -Scope $KV.ResourceId -ObjectId $Identity.PrincipalId -RoleDefinitionName 'Key Vault Contributor' #For UAI
 
 
 $rules          = ConvertFrom-MdTable -MarkdownFilePath .\3TierApp\NsgRules.md                                                  #NSG Creation
@@ -28,4 +24,17 @@ $NSG            = New-AzNetworkSecurityGroup -Name ('tiered' + (Get-Suffix)) @Az
                                                                                             
 $SubnetConfigs  = @{DataTier = '192.168.0.0/29'; AppTier = '192.168.0.8/29'; WebTier = '192.168.0.16/29'}.GetEnumerator() |     #Vnet creation
                     ForEach-Object {New-AzVirtualNetworkSubnetConfig -Name $_.Key -AddressPrefix $_.Value -NetworkSecurityGroup $NSG}
-New-AzVirtualNetwork -Name ('tiered1' + (Get-Suffix)) @AzParams -AddressPrefix 192.168.0.0/27 -Subnet $SubnetConfigs        
+$Vnet           =New-AzVirtualNetwork -Name ('tiered1' + (Get-Suffix)) @AzParams -AddressPrefix 192.168.0.0/27 -Subnet $SubnetConfigs     
+
+$envVars = @{
+    "RG_NAME"   = $RG.ResourceGroupName
+    "SA_NAME"   = $StorageAcc.StorageAccountName
+    "LAW_NAME"  = $LAW.Name 
+    "KV_NAME"   = $KV.VaultName
+    "NSG_NAME"  = $NSG.Name
+    "VNET_NAME" = $Vnet.Name
+}
+
+foreach ($key in $envVars.Keys) {
+    "$key=$($envVars[$key])" | Out-File -FilePath $Env:GITHUB_ENV -Append
+}
