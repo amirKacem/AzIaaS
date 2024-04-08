a4950c538e88f7c3431231cee3803f73b24d07f2
Ayan <ayanmullick@gmail.com>
2024-04-07 20:10:26 -0500

    .

--- GHActionTest/FuncVar.ps1
+++ GHActionTest/FuncVar.ps1
@@ -1,4 +1,4 @@
-function Get-CurrentTime { (Get-Date).ToString("hh:mm:ss tt") }
+#function Get-CurrentTime { (Get-Date).ToString("hh:mm:ss tt") }
 
 <#
 $RG = Get-AzResourceGroup -Name GitHubAction24
@@ -7,4 +7,8 @@ $rgName = $RG.ResourceGroupName
 Write-Output "RG_NAME=$rgName" >> $Env:GITHUB_ENV  #Persists only across 'steps' in the same job. And is deleted after GHWorkflow run
 #>
 
-Write-Output "RG_NAME=$(Get-AzResourceGroup -Name GitHubAction24|ConvertTo-Json -Compress)" >> $Env:GITHUB_ENV
+#Write-Output "RG_NAME=$(Get-AzResourceGroup -Name GitHubAction24|ConvertTo-Json -Compress)" >> $Env:GITHUB_ENV
+
+$RG = Get-AzResourceGroup -Name GitHubAction24 
+$rgName = $RG.ResourceGroupName
+Write-Output "rg_name=$rgName" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
