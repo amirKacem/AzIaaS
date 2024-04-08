edf15dbdf2c28d3a1cb2f49552347649676e1cc7
Ayan <ayanmullick@gmail.com>
2024-04-07 21:58:42 -0500

    .

--- GHActionTest/FuncVar.ps1
+++ GHActionTest/FuncVar.ps1
@@ -7,8 +7,4 @@ $rgName = $RG.ResourceGroupName
 Write-Output "RG_NAME=$rgName" >> $Env:GITHUB_ENV  #Persists only across 'steps' in the same job. And is deleted after GHWorkflow run
 #>
 
-#Write-Output "RG_NAME=$(Get-AzResourceGroup -Name GitHubAction24|ConvertTo-Json -Compress)" >> $Env:GITHUB_ENV
-
-$RG = Get-AzResourceGroup -Name GitHubAction24 
-$rgName = $RG.ResourceGroupName
-Write-Output "rg_name=$rgName" | Out-File -FilePath $Env:GITHUB_OUTPUT -Encoding utf8 -Append
+Write-Output "RG_NAME=$(Get-AzResourceGroup -Name GitHubAction24|ConvertTo-Json -Compress)" >> $Env:GITHUB_ENV
