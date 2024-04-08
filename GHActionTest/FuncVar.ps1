799e91166d152c4357b028da6be9b38edb1921d5
Ayan <ayanmullick@gmail.com>
2024-04-07 23:14:12 -0500

    .

--- GHActionTest/FuncVar.ps1
+++ GHActionTest/FuncVar.ps1
@@ -24,5 +24,16 @@ Write-Output "RG=$(Get-AzResourceGroup -Name GitHubAction24|ConvertTo-Json -Comp
 $rgName = "${{ needs.set_resource_group.outputs.rg }}"
 Write-Host "Using Resource Group Name: $rgName"
 
-$env:needs.set_resource_group.outputs.rg | ConvertFrom-Json  #v2
+${{ needs.set_resource_group.outputs.rg_name }}| ConvertFrom-Json  #v2: It gets the values but results in a parser error
+
+Error: 
+{"ResourceGroupName":"GitHubAction24","Location":"northcentralus","ProvisioningState":"Succeeded","Tags":{},"TagsTable":null,"ResourceId":"/subscriptions/8b77f191-3d4a-498e-8bb1-9e1fbb927ea7/resourceGroups/GitHubAction24","ManagedBy":null}| ConvertFrom-Json
+  shell: /usr/bin/pwsh -command ". '{0}'"
+ParserError: /home/runner/work/_temp/638506b1-b906-4ad1-b33a-0e3d9d0ec1eb.ps1:2
+Line |
+   2 |  {"ResourceGroupName":"GitHubAction24","Location":"northcentralus","Pr …
+     |                      ~~~~~~~~~~~~~~~~~
+     | Unexpected token ':"GitHubAction24"' in expression or statement.
+Error: Process completed with exit code 1.
+
 #>
