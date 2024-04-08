7f0c1cbb6bcf3b3d88f3abef4213a66271274f38
Ayan <ayanmullick@gmail.com>
2024-04-07 22:17:46 -0500

    .

--- GHActionTest/FuncVar.ps1
+++ GHActionTest/FuncVar.ps1
@@ -8,3 +8,15 @@ Write-Output "RG_NAME=$rgName" >> $Env:GITHUB_ENV  #Persists only across 'steps'
 #>
 
 Write-Output "RG_NAME=$(Get-AzResourceGroup -Name GitHubAction24|ConvertTo-Json -Compress)" >> $Env:GITHUB_ENV
+
+
+<# Across Jobs
+#Set
+$RG = Get-AzResourceGroup -Name GitHubAction24 -Verbose
+$rgName = $RG.ResourceGroupName
+Write-Output "rg_name=$rgName" >> $Env:GITHUB_OUTPUT
+
+#Get
+$rgName = "${{ needs.set_resource_group.outputs.rg }}"
+Write-Host "Using Resource Group Name: $rgName"
+#>
