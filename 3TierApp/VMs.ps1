6a470ac14f5c71a06ec24ea66b6e30541b934693
Ayan <ayanmullick@gmail.com>
2025-01-20 22:52:08 -0600

    .

--- 3TierApp/VMs.ps1
+++ 3TierApp/VMs.ps1
@@ -1,17 +1,15 @@
-#$EnvVars      = ConvertFrom-Json '${{ needs.GovernanceResourcesJob.outputs.Env_Vars }}'
 $AzParams     = @{Location = 'NorthCentralUS'; ResourceGroupName  = $($EnvVars.RG_NAME); Verbose=$true}
-
 $VMs          = ConvertFrom-MdTable -MarkdownFilePath .\3TierApp\VMs.md                                                  
 $Cred         = New-Object System.Management.Automation.PSCredential "TestAdmin",$(ConvertTo-SecureString "Passw0rd" -asplaintext -force)
 
 $VMs|ForEach-Object {
     $SubnetId = ((Get-AzVirtualNetwork -ResourceGroupName $($EnvVars.RG_NAME) -Name $($EnvVars.VNET_NAME)).Subnets|Where-Object name -EQ $PSItem.Subnet).Id
-    $NIC      = New-AzNetworkInterface @AzParams -Name ('Tiered' + $Psitem.Name+ (Get-Suffix)) -IpConfiguration $(New-AzNetworkInterfaceIpConfig -Name IPconfig -SubnetId $SubnetId -Primary)
+    $NIC      = New-AzNetworkInterface @AzParams -Name ('Tiered' + $($VmName=$Psitem.Name;$VmName)+ (Get-Suffix)) -IpConfiguration $(New-AzNetworkInterfaceIpConfig -Name IPconfig -SubnetId $SubnetId -Primary)
     $Nic.IpConfigurations[0].PrivateIpAllocationMethod = 'Static'
     $Nic|Set-AzNetworkInterface
 
-    $vmConfig = New-AzVMConfig -VMName $Psitem.Name -VMSize $Psitem.Tier -IdentityType UserAssigned -IdentityId $($EnvVars.ID)|
-       Set-AzVMOperatingSystem -Windows -ComputerName $Psitem.Name -Credential $Cred -TimeZone 'Central Standard Time' -ProvisionVMAgent -EnableAutoUpdate -PatchMode AutomaticByPlatform|
+    $vmConfig = New-AzVMConfig -VMName ('Tiered' + $Psitem.Name+ 'VM') -VMSize $Psitem.Tier -IdentityType UserAssigned -IdentityId $($EnvVars.ID)|
+       Set-AzVMOperatingSystem -Windows -ComputerName ('Tiered' + $Psitem.Name+ 'VM') -Credential $Cred -TimeZone 'Central Standard Time' -ProvisionVMAgent -EnableAutoUpdate -PatchMode AutomaticByPlatform|
        Set-AzVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus '2025-datacenter-azure-edition-core-smalldisk' -Version latest -Verbose|
        Set-AzVMOSDisk -Name ('Tiered' + $Psitem.Name+ (Get-Suffix)) -Caching ReadWrite -CreateOption FromImage|
        Set-AzVMSecurityProfile -SecurityType TrustedLaunch|Set-AzVmUefi -EnableVtpm $true -EnableSecureBoot $true|
@@ -19,7 +17,7 @@ $VMs|ForEach-Object {
 
     $disks    = ($Psitem|Select-Object -Property Drive*).psobject.Properties|Select-Object -Property Name,Value|Where-Object value
     $disks|ForEach-Object{
-        Add-AzVMDataDisk -VM $vmConfig -Name ('Tiered' + $Psitem.Name+ (Get-Suffix)) -Caching ReadWrite -DiskSizeInGB $Psitem.Value -Lun  $([array]::indexof($disks,$PSItem)+1) -CreateOption Empty
+        Add-AzVMDataDisk -VM $vmConfig -Name ('Tiered'+ $VmName + $Psitem.Name+ (Get-Suffix)) -Caching ReadWrite -DiskSizeInGB $Psitem.Value -Lun  $([array]::indexof($disks,$PSItem)+1) -CreateOption Empty
                         }
 
     New-AzVM @AzParams -VM $vmConfig
