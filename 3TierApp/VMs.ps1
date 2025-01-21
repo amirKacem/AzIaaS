e30ca07057137df04394aa3315aab8387076070d
Ayan <ayanmullick@gmail.com>
2025-01-20 22:07:28 -0600

    Refactor environment variable usage in workflows and scripts for improved consistency and clarity

--- 3TierApp/VMs.ps1
+++ 3TierApp/VMs.ps1
@@ -1,10 +1,11 @@
-$AzParams     = @{Location = 'NorthCentralUS'; ResourceGroupName  = $env:RG_NAME; Verbose=$true}
+$EnvVars = ConvertFrom-Json '${{ needs.GovernanceResourcesJob.outputs.Env_Vars }}'
+$AzParams     = @{Location = 'NorthCentralUS'; ResourceGroupName  = $($EnvVars.RG_NAME); Verbose=$true}
 
 $VMs          = ConvertFrom-MdTable -MarkdownFilePath .\3TierApp\VMs.md                                                  
 $Cred         = New-Object System.Management.Automation.PSCredential "TestAdmin",$(ConvertTo-SecureString "Passw0rd" -asplaintext -force)
 
 $VMs|ForEach-Object {
-    $SubnetId = ((Get-AzVirtualNetwork -ResourceGroupName $env:RG_NAME).Subnets|Where-Object name -EQ $PSItem.Subnet).Id
+    $SubnetId = ((Get-AzVirtualNetwork -ResourceGroupName $($EnvVars.RG_NAME) -Name $($EnvVars.VNET_NAME)).Subnets|Where-Object name -EQ $PSItem.Subnet).Id
     $NIC      = New-AzNetworkInterface @AzParams -Name ('Tiered' + $Psitem.Name+ (Get-Suffix)) -IpConfiguration $(New-AzNetworkInterfaceIpConfig -Name IPconfig -SubnetId $SubnetId -Primary)
     $Nic.IpConfigurations[0].PrivateIpAllocationMethod = 'Static'
     $Nic|Set-AzNetworkInterface
@@ -14,7 +15,7 @@ $VMs|ForEach-Object {
        Set-AzVMSourceImage -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus '2025-datacenter-azure-edition-core-smalldisk' -Version latest -Verbose|
        Set-AzVMOSDisk -Name ('Tiered' + $Psitem.Name+ (Get-Suffix)) -Caching ReadWrite -CreateOption FromImage|
        Set-AzVMSecurityProfile -SecurityType TrustedLaunch|Set-AzVmUefi -EnableVtpm $true -EnableSecureBoot $true|
-       Add-AzVMNetworkInterface -Id $NIC.Id|Set-AzVMBootDiagnostic -ResourceGroupName $env:RG_NAME -Enable
+       Add-AzVMNetworkInterface -Id $NIC.Id|Set-AzVMBootDiagnostic -ResourceGroupName $($EnvVars.RG_NAME) -Enable
 
     $disks    = ($Psitem|Select-Object -Property Drive*).psobject.Properties|Select-Object -Property Name,Value|Where-Object value
     $disks|ForEach-Object{
